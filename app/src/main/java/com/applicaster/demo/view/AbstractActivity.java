ebc57119fc34ad7d70b52f673f18497be15d87ba
Rafael <rambruster@gmail.com>
2017-03-11 13:29:29 -0500

    no message

--- app/src/main/java/com/applicaster/demo/view/AbstractActivity.java
+++ app/src/main/java/com/applicaster/demo/view/AbstractActivity.java
@@ -1,16 +1,10 @@
 package com.applicaster.demo.view;
 
-import android.os.AsyncTask;
-import android.os.Build;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-import java.util.LinkedList;
-import java.util.List;
 
 /**
  * Created by Ambruster on 3/10/2017.
- * Abstract Activity with some of the most common used helper method, not necessary all the others
+ * Abstract Activity with some of the most common used helper method.
  * Its not mandatory that activities extend from this, but its a good practice
  */
 
